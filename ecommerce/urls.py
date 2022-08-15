f6cdd9d6d5a5e5c9249fb00ff07a95b4daec3271
radja <rhriman869@gmail.com>
2022-08-15 16:42:34 +0100

    commit

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -5,19 +5,20 @@ from django.conf.urls.static import static
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+
 schema_view = get_schema_view(
    openapi.Info(
-      title="ecommerce API",
-<<<<<<< HEAD
+      title="Ecommerce API",
       default_version='v1',
-||||||| 1eaf559
-=======
-       default_version='v1',
->>>>>>> c3f912d0c1f8319141fce352bbd65758aa3a2bfa
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
    ),
    public=True,
    permission_classes=[permissions.AllowAny],
 )
+
 
 
 urlpatterns = [
