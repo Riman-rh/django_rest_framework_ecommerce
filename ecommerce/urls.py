33098b42423fc6c0ed88a456507d5f7b1a852860
radja <rhriman869@gmail.com>
2022-08-15 09:40:52 +0100

    commit

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -8,6 +8,7 @@ from drf_yasg import openapi
 schema_view = get_schema_view(
    openapi.Info(
       title="ecommerce API",
+      default_version='v1',
    ),
    public=True,
    permission_classes=[permissions.AllowAny],
@@ -20,4 +21,4 @@ urlpatterns = [
     path('v3', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls'))
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
